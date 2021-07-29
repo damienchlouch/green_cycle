a42f3a96f0379cf18e2c3b72c16a7e96ed838eb6
Damien <damienlaouch@gmail.com>
2021-07-29 16:52:12 +0200

    Merci Rudy

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let cssModal = `
     left : 0;
     width: 100%;
     height: 100vh;
-    background-color: rgba(54, 54, 54, 0.192);;
+    background-color: rgba(255, 255, 255, 0.85);
 `;
 
 document.querySelectorAll("[modal]").forEach(element => {
